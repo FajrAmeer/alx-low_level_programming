41ac6d211e1fb355a2540d6701a05390d81996dd
FajrAmeer <fajrameer@gmail.com>
2023-08-10 19:37:44 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
@@ -12,4 +12,4 @@ int main (void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(double));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-}
+i}
